f9f3c3a0de90e3a816a652ef4b39e8fa8f56e884
Sagar <sagarpatra641@gmail.com>
2023-11-24 02:42:08 +0530

    clean code

--- script.js
+++ script.js
@@ -10,47 +10,6 @@ textDelete.addEventListener("click", function(){
     smallHeading.remove();
 });
 
-// contactText.addEventListener("click", function(){
-//   // c.style.filter = "blur(5px)";
-//   // body.style.filter = blur("2px");
-//   // document.body.style.filter = "blur(5px)";
-//   document.querySelector(".popup-container").style.display = "block";
-//   // showPopup.classList.add("active");
-//   overlay.classList.add("overlayactive");
-// });
-
-closePopup.addEventListener("click", function(){
-  document.querySelector(".popup-container").style.display = "none";
-  showPopup.classList.remove("active");
-  overlay.classList.remove("overlayactive");
-});
-
-// function blur(){
-//   document.body.style.filter = "blur(5px)";
-// };
-
-
-
-
-/* Menu JS */
-/*
-const mainMenu = document.querySelector('.mainMenu');
-const openMenu = document.querySelector('.menu-btn');
-const closeMenu = document.querySelector('.closeMenu');
-
-openMenu.addEventListener("click", show());
-closeMenu.addEventListener("click", close());
-
-function show(){
-    mainMenu.style.display = 'flex';
-    // mainMenu.style.top = '';
-};
-
-function close(){
-    mainMenu.style.top = '-100%';
-};
-*/
-
 /* Swipper slider JS */
 
 var swiper = new Swiper(".mySwiper", {
